ecc0c83cd40f886d7d8bc1f0eebbabefd3bba153
FenrirRM <rehoudja.mael@gmail.com>
2023-11-22 03:13:54 +0100

    sass, Header, Footer, 404

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return(
+        <footer className="footer">
+            <img src="/assets/logo-footer.png" alt="logo de kasa" />
+            <h2>© 2020 Kasa. All rights reserved</h2>
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+
+function Header (){
+    return (
+        <header className="header">
+            <img  src="/assets/LOGO.png" alt="logo de kasa" />
+            <nav>
+            <NavLink to="/" className={(link) => (link.isActive ? "active" : "")}>Acceuil</NavLink>
+            <NavLink to="/About" className={(link) => (link.isActive ? "active" : "")}>A Propos</NavLink>
+            </nav>
+        </header>
+        
+
+    )
+}
+
+export default Header;
