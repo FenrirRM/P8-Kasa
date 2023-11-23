af2482ff95cbd6dfc218757590e0b8d03f06f39d
FenrirRM <rehoudja.mael@gmail.com>
2023-11-23 13:57:16 +0100

    Ajouts collapses page about

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import arrowup from "./arrowup.png";
+
+function Collapse ({title, content,}){
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!open);
+    }
+
+    return(
+        <div className="collapse">
+            <div className="collapse_title">
+                <h2>{title}</h2>
+                <img src= {arrowup} alt="flêche collapse" className={ open ? "arrow arrow_down":"arrow arrow_up"} onClick={toggle} />
+            </div>
+            <div className={ open ? "collapse_content collapse_content_show":"collapse_content collapse_content_hide"}>
+                {content}
+            </div>
+        </div>
+    )
+}
+
+export default Collapse
