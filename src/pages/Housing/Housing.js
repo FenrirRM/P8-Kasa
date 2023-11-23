3b3541244a076b8ff132ca7d3a7ccf3bab4bb8d1
FenrirRM <rehoudja.mael@gmail.com>
2023-11-23 18:10:09 +0100

    Correction anim collapse

--- src/pages/Housing/Housing.js
+++ src/pages/Housing/Housing.js
@@ -6,15 +6,19 @@ import Slideshow from "../../components/Slideshow/Slideshow";
 function Housing () {
     const {id} = useParams();
     const housing = Data.find((house) => house.id === id);
+    const { tags, equipments, title, location, rating, host, description } =
+      housing;
 
-
+    const equipmentsList = equipments.map((equipment) => (
+        <li key={equipment}>{equipment}</li>
+      ));
 
     return(
         <main className="housing">
             <Slideshow pictures={housing.pictures}/>
             <div className="housing_collapse">
             <Collapse title="Description" content={housing.description} />
-            <Collapse title="Équipements" content={housing.equipments} />
+            <Collapse title="Équipements" content={equipmentsList} />
             </div>
         </main>
     )
