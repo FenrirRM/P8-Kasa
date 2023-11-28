4068cd542e3ffbd981c1176b69ba768be65126fb
FenrirRM <rehoudja.mael@gmail.com>
2023-11-28 01:52:09 +0100

    Ajout Rating

---
+++ src/components/Rating/Rating.jsx
@@ -0,0 +1,20 @@
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+function Rating ({rating}) {
+    const maxRating = 5;
+    const stars = [];
+
+    for (let i = 1; i <= maxRating; i++) {
+        if (i <= rating) {
+          stars.push(<FontAwesomeIcon icon={faStar} className="star_red" key={i} />);
+        } else {
+          stars.push(<FontAwesomeIcon icon={faStar} className="star" key={i} />);
+        }
+    }    
+
+    return <div className="rating">{stars}</div>;
+}
+
+export default Rating;
--- src/pages/Housing/Housing.js
+++ src/pages/Housing/Housing.js
@@ -1,27 +1,64 @@
 import { useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Data from "../../logements.json";
 import Collapse from "../../components/Collapse/Collapse";
 import Slideshow from "../../components/Slideshow/Slideshow";
+import Rating from "../../components/Rating/Rating";
 
-function Housing () {
-    const {id} = useParams();
-    const housing = Data.find((house) => house.id === id);
-    const { tags, equipments, title, location, rating, host, description } =
-      housing;
+function Housing() {
+  const { id } = useParams();
+  const housing = Data.find((house) => house.id === id);
 
-    const equipmentsList = equipments.map((equipment) => (
-        <li key={equipment}>{equipment}</li>
-      ));
+  if (housing === undefined) {
+    return <Navigate to="*" />;
+  }
 
-    return(
-        <main className="housing">
-            <Slideshow pictures={housing.pictures}/>
-            <div className="housing_collapse">
-            <Collapse title="Description" content={housing.description} />
-            <Collapse title="Équipements" content={equipmentsList} />
-            </div>
-        </main>
-    )
+  const {
+    tags,
+    equipments,
+    title,
+    location,
+    rating,
+    host: { name, picture },
+    description,
+  } = housing;
+
+  const equipmentsList = equipments.map((equipment) => (
+    <li key={equipment}>{equipment}</li>
+  ));
+
+  const tagList = tags.map((tag) => (
+    <li className="tags" key={tag}>
+      {tag}
+    </li>
+  ));
+
+  
+
+  return (
+    <main>
+      <Slideshow pictures={housing.pictures} />
+      <div className="housing">
+        <div className="housing_info">
+          <h1 className="housing_info_title">{title}</h1>
+          <p className="housing_info_location">{location}</p>
+          <ul>{tagList}</ul>
+        </div>
+        <div className="housing_host-rating">
+          <div className="housing_host-rating_host">
+            <p>{name}</p>
+            <img src={picture} alt="" />
+          </div>
+          <Rating rating={rating} />
+        </div>
+      </div>
+
+      <div className="housing_collapse">
+        <Collapse title="Description" content={description} />
+        <Collapse title="Équipements" content={equipmentsList} />
+      </div>
+    </main>
+  );
 }
 
 export default Housing;
